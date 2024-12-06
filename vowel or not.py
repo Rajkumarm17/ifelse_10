03997114ac3727c0c1e00704e2053c932aeb8657
Rajkumar17 <rajkumarm1703@gmail.com>
2024-12-06 18:05:37 +0530

    vowel or consonent

---
+++ vowel or not.py
@@ -0,0 +1,6 @@
+# vowel or not
+n = input(" enter an alphabet :")
+if n in ('a', 'e', 'i', 'o', 'u') or n in ('A', 'E', 'I', 'O', 'U'):
+    print("an alphabet is vowel")
+else:
+    print("an alphabet is consonent")
